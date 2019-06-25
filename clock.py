8ba9c590473cea7f52ee0282faced12d2965696b
gabriel <gabrielgoller77@gmail.com>
2019-06-25 15:55:46 +0200

    prepared code for test
    prepared code for stepper motor, link for servo and sensors.

---
+++ clock.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+# stepper doesnt need resistor
+stepper_pins = [7,11,13,15]
+delay = 0.001
+
+def stepper(stepper_pins, delay):
+    for pin in stepper_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
+
+    steps_seq = [[1,0,0,0], [1,1,0,0], [0,1,0,0], [0,1,1,0], [0,0,1,0], [0,0,1,1], [0,0,0,1], [1,0,0,1]]
+
+    for step in range(8):
+        for pin in range(4):
+            GPIO.output(stepper_pins[pin], steps_seq[step][pin])
+        time.sleep(delay)
+
+
+#one revolution 512 (not approved)
+for i in range(512):
+    stepper(stepper_pins, delay)
+
+# https://elinux.org/RPi_GPIO_Interface_Circuits#Buttons_and_switches
+# reed switch needs resistor!!
+# avoid sensor needs resistor!!
+# button needs resistor!!
+# Servos:
+# https://tutorials-raspberrypi.de/raspberry-pi-servo-motor-steuerung/
