ea2af6d5925f10886f4aebba2f4be64eca5e5665
gabriel <gabrielgoller77@gmail.com>
2019-06-26 14:25:05 +0200

    added logging to external file
    added try, catch log errors to file

--- clock.py
+++ clock.py
@@ -1,11 +1,22 @@
 import RPi.GPIO as GPIO
 import time
+import logging
+
+
+GPIO.setmode(GPIO.BOARD)
+logging.basicConfig(filename='raspi.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
+
+stepper_pins = [7,11,13,15]
+delay = 0.001
 
 
 def stepper(stepper_pins, delay):
     for pin in stepper_pins:
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, 0)
+        try:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, 0)
+        except Exception as e:
+            logging.error("stepper_motor pin %d no signal", pin)
 
     steps_seq = [[1,0,0,0], [1,1,0,0], [0,1,0,0], [0,1,1,0], [0,0,1,0], [0,0,1,1], [0,0,0,1], [1,0,0,1]]
 
@@ -15,14 +26,9 @@ def stepper(stepper_pins, delay):
         time.sleep(delay)
 
 def main():
-    #one revolution 512 (not approved)
+    # one revolution 512 (to check!)
     for i in range(512):
         stepper(stepper_pins, delay)
-
-    GPIO.setmode(GPIO.BOARD)
-    # stepper doesnt need resistor
-    stepper_pins = [7,11,13,15]
-    delay = 0.001
 
 if __name__ == '__main__':
     main()
@@ -31,5 +37,6 @@ if __name__ == '__main__':
 # reed switch needs resistor!!
 # avoid sensor needs resistor!!
 # button needs resistor!!
+# stepper doesn't need resistor!!
 # Servos:
 # https://tutorials-raspberrypi.de/raspberry-pi-servo-motor-steuerung/
